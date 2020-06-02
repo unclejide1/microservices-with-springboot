9fb6aebea96177d0939c2105bcf7a930f365ac56
Jideofor <njusticej@gmail.com>
2020-06-02 13:02:05 +0100

    feat: set up structure for gallery service

---
+++ gallery-service/src/main/java/com/microservices/galleryservice/GalleryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.galleryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryServiceApplication.class, args);
+    }
+
+}
---
+++ gallery-service/src/test/java/com/microservices/galleryservice/GalleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.galleryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
